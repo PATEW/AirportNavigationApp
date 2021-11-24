c2cea5723cd27a643a925a1cb910c29d30dfd369
Kgarber21 <Garberk@my.erau.edu>
2021-11-24 13:05:12 -0500

    Checklist page updates including user item adding functionality

--- AirportNavigationApp/AirportNavigationApp/Views/CheckListPage.xaml.cs
+++ AirportNavigationApp/AirportNavigationApp/Views/CheckListPage.xaml.cs
@@ -15,6 +15,7 @@ namespace AirportNavigationApp.Views
 
         public Grid checklist;
         public List<string> checklistItems;
+        public Entry newItem;
 
         public CheckListPage()
         {
@@ -26,7 +27,17 @@ namespace AirportNavigationApp.Views
 
         public void CreateChecklist()
         {
-            checklist = new Grid();
+
+            checklist = new Grid
+            {
+                ColumnDefinitions = {
+                    new ColumnDefinition { Width = new GridLength(0.85, GridUnitType.Star) },
+                    new ColumnDefinition { Width = new GridLength(0.15, GridUnitType.Star) },
+                },
+                ColumnSpacing = 0,
+                RowSpacing = 0,
+            };
+
             checklistItems = new List<string>();
 
             checklistItems.Add("Park at Airport");
@@ -43,13 +54,13 @@ namespace AirportNavigationApp.Views
 
                 var arrivedLabel = new Label
                 {
-                    Text = checklistItems[i],
+                    Text = " " + (i+1) + ". " + checklistItems[i],
                     VerticalOptions = LayoutOptions.Center,
-                    HorizontalOptions = LayoutOptions.Center,
                     FontSize = 20,
                     TextColor = Color.White,
+                    Padding = new Thickness(0, 10, 0, 10),
                 };
-                checklist.Children.Add(arrivedLabel, 1, i);
+                checklist.Children.Add(arrivedLabel, 0, i);
 
                 CheckBox checkBox = new CheckBox
                 {
@@ -58,9 +69,88 @@ namespace AirportNavigationApp.Views
                     Color = Color.FromHex("#00A9CD"),
                 };
                 checkBox.CheckedChanged += onCheck;
-                checklist.Children.Add(checkBox, 0, i);
+                checklist.Children.Add(checkBox, 1, i);
+
+                BoxView border = new BoxView {
+                    HeightRequest = 1,
+                    Color = Color.White,
+                    VerticalOptions = LayoutOptions.End ,
+                };
+                BoxView border1 = new BoxView
+                {
+                    HeightRequest = 1,
+                    Color = Color.White,
+                    VerticalOptions = LayoutOptions.End,
+                };
+                checklist.Children.Add(border, 0, i);
+                checklist.Children.Add(border1, 1, i);
             }
+            addItemRow();
+
             Checklist.Content = checklist;
+        }
+
+        public void addItemRow()
+        {
+            newItem = new Entry
+            {
+                Placeholder = "Add A Custom Item",
+                PlaceholderColor = Color.White,
+                TextColor = Color.White,
+                FontSize = 20,
+                Margin = new Thickness(25, 10, 0, 10),
+
+            };
+            Button newItemButton = new Button
+            {
+                Text = "Add",
+            };
+            newItemButton.Clicked += newItemButtonClicked;
+            checklist.Children.Add(newItem, 0, checklistItems.Count);
+            checklist.Children.Add(newItemButton, 1, checklistItems.Count);
+
+            BoxView border2 = new BoxView
+            {
+                HeightRequest = 1,
+                Color = Color.White,
+                VerticalOptions = LayoutOptions.End,
+            };
+            BoxView border3 = new BoxView
+            {
+                HeightRequest = 1,
+                Color = Color.White,
+                VerticalOptions = LayoutOptions.End,
+            };
+            checklist.Children.Add(border2, 0, checklistItems.Count);
+            checklist.Children.Add(border3, 1, checklistItems.Count);
+        }
+
+        private void newItemButtonClicked(object sender, EventArgs e)
+        {
+            checklistItems.Add(newItem.Text);
+            checklist.Children.RemoveAt(checklist.Children.Count - 3);
+            checklist.Children.RemoveAt(checklist.Children.Count - 3);
+
+            var arrivedLabel = new Label
+            {
+                Text = newItem.Text,
+                VerticalOptions = LayoutOptions.Center,
+                FontSize = 20,
+                TextColor = Color.White,
+                Padding = new Thickness(25, 10, 0, 10),
+            };
+            checklist.Children.Add(arrivedLabel, 0, checklistItems.Count - 1);
+
+            CheckBox checkBox = new CheckBox
+            {
+                VerticalOptions = LayoutOptions.Center,
+                HorizontalOptions = LayoutOptions.Center,
+                Color = Color.FromHex("#00A9CD"),
+            };
+            checkBox.CheckedChanged += onCheck;
+            checklist.Children.Add(checkBox, 1, checklistItems.Count - 1);
+
+            addItemRow();
         }
 
         public void onCheck(object sender, EventArgs e) {
